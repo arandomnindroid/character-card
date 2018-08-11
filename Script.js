ed87b08764ba25eec2bbdec1640bf89b4f126550
arandomnindroid <MCNindroidsp@outlook.com>
2018-08-11 15:18:40 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -5,7 +5,7 @@ var imageObj2 = new Image();
 imageObj1.src = "407139CA-64EF-4416-9DA7-194AD061C858.png"
 imageObj1.onload = function() {
    ctx.drawImage(imageObj1, 0, 0, 328, 526);
-   imageObj2.src = "";
+   imageObj2.src = "842C5DC6-C713-41A5-BF2B-616230CAD395.png";
    imageObj2.onload = function() {
       ctx.drawImage(imageObj2, 15, 85, 300, 300);
       var img = c.toDataURL("image/png");
